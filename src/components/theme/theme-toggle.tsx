618a5d8c4d6422f638c5bd63d49f5a8a524bfeff
Mohamed Landolsi <mohamedlandolsi300@gmail.com>
2025-04-04 15:50:52 +0100

    Deployment

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
